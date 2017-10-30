15780dfcaf24053cdff27acb5c68db570fd1bf3a
s1lent <s1lentsk@yandex.ru>
2017-10-31 01:17:02 +0700

    Fix typo

--- revoice/src/revoice_rehlds_api.cpp
+++ revoice/src/revoice_rehlds_api.cpp
@@ -37,7 +37,7 @@ bool Revoice_RehldsApi_TryInit(CSysModule *engineModule, char *failureReason)
 	}
 
 	if (minorVersion < REHLDS_API_VERSION_MINOR) {
-		sprintf(failureReason, "REHLDS Api minor version mismatch; expected at least %d, real %d\n", REHLDS_API_VERSION_MINOR, majorVersion);
+		sprintf(failureReason, "REHLDS Api minor version mismatch; expected at least %d, real %d\n", REHLDS_API_VERSION_MINOR, minorVersion);
 		return false;
 	}
 
